160585a9656c66ed1a0d1481ba2dbd65f6447ddf
nc-minh <nguyencongminh.official@gmail.com>
2023-06-30 00:18:32 +0700

    10d

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -11,10 +11,13 @@ dotenv.config({ path: pathEnv });
 export default {
   port: process.env.PORT || 8000,
   mongodb: {
+    protocol: process.env.MONGODB_PROTOCOL,
     username: process.env.MONGODB_USERNAME,
     pasword: process.env.MONGODB_PASSWORD,
     host: process.env.MONGODB_HOST,
+    replicaSet: process.env.MONGODB_REPLICA_SET,
     dbName: process.env.MONGODB_NAME,
+    uri: process.env.MONGODB_URI,
   },
 
   secret: {
--- src/resources/mongo.ts
+++ src/resources/mongo.ts
@@ -1,21 +1,48 @@
-import config from 'configs';
-import mongoose, { connect, connection, set } from 'mongoose';
+import { connect, connection } from 'mongoose';
 
-const mongoDBConfig = config.mongodb;
+import configs from 'configs';
+
+const mongoDBConfig = configs.mongodb;
+
+const mongodbProtocol = mongoDBConfig.protocol || 'mongodb';
 
 const userNamePwd = mongoDBConfig.username
-  ? `${mongoDBConfig.username}:${mongoDBConfig.pasword}`
+  ? `${mongoDBConfig.username}:${mongoDBConfig.pasword}@`
   : '';
 
-let mongodbUrl = `mongodb+srv://${userNamePwd}@${mongoDBConfig.host}/${mongoDBConfig.dbName}?retryWrites=true&w=majority`;
+let mongodbUrl = `${mongodbProtocol}://${userNamePwd}${mongoDBConfig.host}/${mongoDBConfig.dbName}?retryWrites=true`;
+
+if (mongoDBConfig.replicaSet) {
+  mongodbUrl += `&replicaSet=${mongoDBConfig.replicaSet}`;
+}
+
+const options = {
+  autoIndex: true,
+  autoCreate: true,
+};
 
 export default async () => {
   try {
-    set('strictQuery', false);
-    await connect(mongodbUrl, { autoIndex: true, autoCreate: true });
-    console.log('Connect MongoDB successfully');
-  } catch (e) {
-    console.log('Connection Error', e);
-    process.exit(1);
+    console.log('Starting connect to MongoDB...');
+
+    await connect(mongodbUrl, options);
+
+    connection.on('connected', function () {
+      console.log('MongoDB::: Successfully connected to MongoDB');
+    });
+
+    connection.on('disconnected', function () {
+      console.log('\nMongoDB::: Disconnected');
+    });
+
+    connection.on('error', (error) => {
+      console.log('MongoDB::: Connection error::::', JSON.stringify(error));
+
+      process.exit(1);
+    });
+
+    console.log('Successfully connected to MongoDB');
+  } catch (error) {
+    console.log(`MongoDB::: Error in tryCatch::: ${error}`);
   }
 };
